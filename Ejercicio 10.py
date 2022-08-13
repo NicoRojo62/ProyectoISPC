b5b2fda424056b44276ca2750fdac4fde85dba25
veroivk <veronicakostich@gmail.com>
2022-08-13 04:21:41 -0300

    Valoracion Final Ejercicio 10 en Python

---
+++ Ejercicio 10.py
@@ -0,0 +1,8 @@
+#Crear una lista denominada "Clientes" con los nombres de los diferentes dueños de perros.
+
+Clientes = ["Juan", "Mario", "Ariel", "Josefina", "Marianella"]
+
+#Ordenarla alfabéticamente y mostrarla por pantalla.
+
+Clientes.sort()
+print(Clientes)
