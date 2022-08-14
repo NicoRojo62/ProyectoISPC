71649774ae8eab3a0daea28e24d134d3bbd05cbc
NicoRojo62 <106413642+NicoRojo62@users.noreply.github.com>
2022-08-14 03:49:39 -0300

    Ejercicio 9
    Phyton - Valoración final módulo programador

---
+++ Ejercicio 9 Rojo Nicolas.py
@@ -0,0 +1,7 @@
+﻿historial5 = (8520, 9510, 7530, 3570, 1590) #se declara la tupla
+montomax = historial5[0] #igualo variable montomax a tupla posición 0
+for var in historial5[1:]: #bucle para revisar la tupla desde posición 1 al final
+    if var > montomax:
+        montomax = var
+        
+print("el monto máximo gastado en Toto es:", montomax, "pesos")
